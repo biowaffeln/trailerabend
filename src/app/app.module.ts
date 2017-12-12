45c379c599909572e5ead4e91a0a20cee839849f
mark <kvetny@w11k.de>
2017-12-12 17:27:54 +0100

    set up login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './routes/login/login.module';
+import { MatToolbarModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/login/login.module.ts
+++ src/app/routes/login/login.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule,
+    MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule
   ],
-  declarations: []
+  declarations: [LoginComponent]
 })
 export class LoginModule { }
