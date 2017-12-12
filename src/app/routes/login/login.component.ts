2aed8b27ae37a72581414ddbe158e13bc17bb409
mark <kvetny@w11k.de>
2017-12-12 17:37:14 +0100

    added a formControl to Login

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  nameForm: FormControl;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.nameForm = new FormControl('', {
+      validators: Validators.required
+    });
+  }
+
+  signIn(name: string) {
+    console.log(`Oh hi, ${name}!`);
   }
 
 }
--- src/app/routes/login/login.module.ts
+++ src/app/routes/login/login.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule,
-    MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule
+    MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule,
+    FormsModule, ReactiveFormsModule
   ],
   declarations: [LoginComponent]
 })
