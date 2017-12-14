d79db22b63b017cb3f57cb99de92fb2f734269cf
Mark Kvetny <m.kvetny@outlook.com>
2017-12-14 22:57:48 +0100

    added admin guards

--- src/app/guards/guards.module.ts
+++ src/app/guards/guards.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { UserGuard } from './user.guard';
 import { VisitorGuard } from './visitor.guard';
 import { RouterModule } from '@angular/router';
+import { AdminGuard } from './admin.guard';
+import { NoAdminGuard } from './no-admin.guard';
 
 @NgModule({
   imports: [
     CommonModule, RouterModule
   ],
   declarations: [],
-  providers: [UserGuard, VisitorGuard]
+  providers: [UserGuard, VisitorGuard, AdminGuard, NoAdminGuard]
 })
 export class GuardsModule { }
---
+++ src/app/guards/no-admin.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../services/auth-service/auth.service';
+import { pipe } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable()
+export class NoAdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.authState.pipe(
+      map(user =>!user || user.email === null),
+      tap(noAdmin => {
+        if (!noAdmin) {
+          this.router.navigate(['admin', 'dashboard']);
+        }
+      })
+    );
+  }
+}
