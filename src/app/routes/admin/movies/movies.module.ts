bb22b8d256a1d77182df85aac8d4908d675b5ae9
Mark Kvetny <m.kvetny@outlook.com>
2017-12-22 20:25:15 +0100

    moved stuff related to movie into movie-directory

--- src/app/routes/admin/movies/movies.module.ts
+++ src/app/routes/admin/movies/movies.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './movies.component';
-import { MovieListModule } from '../dashboard/movie-list/movie-list.module';
-import { AddMovieModule } from '../dashboard/add-movie/add-movie.module';
+import { AddMovieModule } from './add-movie/add-movie.module';
+import { MovieListModule } from './movie-list/movie-list.module';
 
 @NgModule({
   imports: [
