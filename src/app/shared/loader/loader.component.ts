1b44509e202ad3ede6dd5b8fad295c403b282cd2
Mark Kvetny <m.kvetny@outlook.com>
2017-12-22 23:31:17 +0100

    wrapped ng-content in a template
    now it only gets displayed when data is loaded

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class LoaderComponent implements OnInit {
 
   @Input() data: any;
+  @ContentChild(TemplateRef) contentRef;
   
   constructor() { }
 
