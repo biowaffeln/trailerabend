81ff775678899bcd33e096568f69584f62efb018
Mark Kvetny <m.kvetny@outlook.com>
2017-12-22 19:34:27 +0100

    created loader

---
+++ src/app/shared/loader/loader.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader.component';
+import { MatProgressBarModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatProgressBarModule
+  ],
+  declarations: [LoaderComponent]
+})
+export class LoaderModule { }
