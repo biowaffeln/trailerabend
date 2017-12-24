355dfb6174aff12b7cfd034e6ff2578d465ce8d0
Mark Kvetny <m.kvetny@outlook.com>
2017-12-24 18:54:17 +0100

    refactored votingForm creation

--- src/app/routes/vote/voting.component.ts
+++ src/app/routes/vote/voting.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services/auth-service/auth.service';
 import * as firebase from 'firebase/app';
-import { take, takeUntil } from 'rxjs/operators';
+import { take, takeUntil, tap } from 'rxjs/operators';
 import { FirestoreService } from '../../services/firestore.service';
 import { Movie, MovieVote } from '../../models/movie.model';
 import { Subject } from 'rxjs';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import { Vote } from '../../models/votes.model';
+import { switchMap } from 'rxjs/operators/switchMap';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-voting',
@@ -15,40 +17,41 @@ import { Vote } from '../../models/votes.model';
 })
 export class VotingComponent implements OnInit, OnDestroy {
 
-  user: firebase.User;
+  user: User;
   votingForm: FormGroup;
   private onDestroy$ = new Subject<void>();
 
   constructor(private authService: AuthService, private db: FirestoreService) {
   }
 
-  ngOnInit() {
-    this.authService.authState.pipe(take(1))
-      .subscribe(user => {
-        this.user = user;
-      });
+  async ngOnInit() {
 
     this.votingForm = new FormGroup({
-      movies: new FormArray(
-        []
-      )
+      movies: new FormArray([])
     });
-
-    this.db.col$<Movie>('/movies').pipe(
+    this.votingForm.valueChanges.pipe(
       takeUntil(this.onDestroy$)
-    ).subscribe(movies => {
-      this.votingForm = new FormGroup({
-        movies: new FormArray(
-          movies.map(movie => {
-            const vote: MovieVote = {
-              ...movie,
-              vote: Vote.NEUTRAL
-            }
-            return new FormControl(vote);
-          })
-        )
+    ).subscribe(val => console.log(val));
+
+    this.authService.authState.pipe(
+      switchMap(auth => this.db.doc$<User>(`/users/${auth.uid}`)),
+      tap(user => this.user = user),
+      switchMap(() => this.db.col$<Movie>('/movies')),
+      takeUntil(this.onDestroy$)
+    )
+      .subscribe(movies => {
+        this.votingForm.setControl('movies',
+          new FormArray(
+            movies.map(movie => {
+              const vote: MovieVote = {
+                ...movie,
+                vote: Vote.NEUTRAL
+              }
+              return new FormControl(vote);
+            })
+          )
+        );
       });
-    });
   }
 
   get movies(): FormArray {
