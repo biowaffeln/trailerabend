9b335a24181fb3b43cb66e82d80f50949b46aa27
Mark Kvetny <m.kvetny@outlook.com>
2017-12-24 14:46:59 +0100

    added a form to vote-component

--- src/app/routes/vote/vote.component.ts
+++ src/app/routes/vote/vote.component.ts
@@ -1,27 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services/auth-service/auth.service';
 import * as firebase from 'firebase/app';
-import { take } from 'rxjs/operators';
+import { take, takeUntil } from 'rxjs/operators';
+import { FirestoreService } from '../../services/firestore.service';
+import { Movie } from '../../models/movie.model';
+import { Subject } from 'rxjs';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
   styleUrls: ['./vote.component.scss']
 })
-export class VoteComponent implements OnInit {
+export class VoteComponent implements OnInit, OnDestroy {
 
   user: firebase.User;
+  votingForm: FormGroup;
+  private onDestroy$ = new Subject<void>();
 
-  constructor(private authService: AuthService) {
-    this.authService.authState.pipe(
-      take(1)
-    )
-    .subscribe(user => {
-      this.user = user;
-    });
+  constructor(private authService: AuthService, private db: FirestoreService) {
   }
 
   ngOnInit() {
+    this.authService.authState.pipe(take(1))
+      .subscribe(user => {
+        this.user = user;
+      });
+
+    this.votingForm = new FormGroup({
+      movies: new FormArray(
+        []
+      )
+    });
+
+    this.db.col$<Movie>('/movies').pipe(
+      takeUntil(this.onDestroy$)
+    ).subscribe(movies => {
+      this.votingForm = new FormGroup({
+        movies: new FormArray(
+          movies.map(movie => {
+            return new FormControl(movie.name);
+          })
+        )
+      });
+    });
+  }
+
+  get movies(): FormArray { 
+    return this.votingForm.get('movies') as FormArray; 
+ }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 
 }
--- src/app/routes/vote/vote.module.ts
+++ src/app/routes/vote/vote.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { VoteRoutingModule } from './vote-routing.module';
 import { VoteComponent } from './vote.component';
 import { GuardsModule } from '../../guards/guards.module';
+import { VotingCardModule } from './voting-card/voting-card.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     VoteRoutingModule,
+    VotingCardModule,
+    FormsModule, ReactiveFormsModule,
     GuardsModule
   ],
   declarations: [VoteComponent]
