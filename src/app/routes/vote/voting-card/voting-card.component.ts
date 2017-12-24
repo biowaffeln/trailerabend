3f3e76063d5ebf47e91fd87f8d5719afd33d9fba
Mark Kvetny <m.kvetny@outlook.com>
2017-12-24 15:10:49 +0100

    implemented ControlValueAccessor on voting-card

--- src/app/routes/vote/voting-card/voting-card.component.ts
+++ src/app/routes/vote/voting-card/voting-card.component.ts
@@ -1,11 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-voting-card',
   templateUrl: './voting-card.component.html',
-  styleUrls: ['./voting-card.component.scss']
+  styleUrls: ['./voting-card.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => VotingCardComponent),
+      multi: true
+    }
+  ]
 })
-export class VotingCardComponent implements OnInit {
+export class VotingCardComponent implements OnInit, ControlValueAccessor {
+
+  vote: string;
+  propagateChange = (_: any) => {};
+
+  writeValue(value: string | null) {
+    if (value !== undefined) {
+      this.vote = value;
+    }
+  }
+  
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any) {}
 
   constructor() { }
 
