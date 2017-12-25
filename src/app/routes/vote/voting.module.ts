f06365fe68c5263d66d80d73b936bbcc48b392f5
Mark Kvetny <m.kvetny@outlook.com>
2017-12-25 16:10:44 +0100

    added a submit button
    doesn't do anything yet

--- src/app/routes/vote/voting.module.ts
+++ src/app/routes/vote/voting.module.ts
@@ -7,12 +7,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { VotingComponent } from './voting.component';
 import { VotingRoutingModule } from './voting-routing.module';
 import { LoaderModule } from '../../shared/loader/loader.module';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     VotingRoutingModule,
     VotingCardModule,
+    MatButtonModule,
     FormsModule, ReactiveFormsModule,
     LoaderModule,
     GuardsModule
