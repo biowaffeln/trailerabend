6b82973124e7c0f4938dc644366475673aa4d7a3
Mark Kvetny <m.kvetny@outlook.com>
2018-01-27 13:05:39 +0100

    added firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQz-iDPyaeyDSwa74_CpWRrShts4f5_yw",
+    authDomain: "filmrausch-trailerabend.firebaseapp.com",
+    databaseURL: "https://filmrausch-trailerabend.firebaseio.com",
+    projectId: "filmrausch-trailerabend",
+    storageBucket: "filmrausch-trailerabend.appspot.com",
+    messagingSenderId: "423650946004"
+  }
 };
